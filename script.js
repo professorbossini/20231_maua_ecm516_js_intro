7c98e479f279b7c5a202810ae72b792db7c98ac0
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-25 09:27:26 -0300

    apostila02(js): fim do codigo 1.4.1

---
+++ script.js
@@ -0,0 +1,72 @@
+
+
+// console.log(true == 1)
+// console.log(1 == [1])
+// console.log(null == null)
+// console.log(null == undefined)
+// console.log([] == false)
+// console.log([] == [])
+// console.log(new Object() == new Object())
+
+
+// console.log(1 == 1)
+// console.log(1 == "1")
+// console.log(1 === 1)
+// console.log(1 === '1')
+
+
+// const n1 = 2
+// const n2 = '3'
+// //coerção implícita
+// const n3 = n1 + n2
+// //coerção explícita
+// const n4 = n1 + Number(n2)
+// int a;
+// let a
+
+// var a;
+
+// @Deprecated
+
+// Thread.stop()
+// var idade = 18
+// // console.log ("Oi, " + nome)
+// if (idade >= 18){
+//   let nome = "João"//içamento hoist
+//   console.log (nome + ", você pode dirigir")
+// }
+// console.log("Até mais, " + nome)
+
+
+// var linguagem = "Javascript"
+// console.log ("Aprendendo " + linguagem)
+// var linguagem = "Java"
+// console.log("Aprendendo " + linguagem)
+
+
+// var endereco = "Rua C"
+// console.log(endereco)
+// endereco = "Rua B"
+// console.log(endereco)
+// console.log(typeof(endereco))
+
+//ECMAScript2015
+//const, let, var
+// let a = 2
+// let b = "abc"
+// console.log(typeof(b))
+// b = 12
+// console.log(typeof(b))
+// b = true
+// console.log(typeof(b))
+// const nome1 = "Ana"
+// const nome2 = 'Pedro'
+// const nome3 = `José`
+
+// console.log(typeof(nome1))
+// console.log(typeof(nome2))
+// console.log(typeof(nome3))
+
+// nome1 = "João"
+
+//REPL: Read, Evaluate, Print, Loop
